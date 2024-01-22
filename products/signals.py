361892df6fe92a806353ac924651fc38d0a4a0fb
JefferDevop <somadesarrollo@gmail.com>
2024-01-22 02:34:04 -0500

    signal syn products

--- products/signals.py
+++ products/signals.py
@@ -7,48 +7,51 @@ from customers.models import Product_public
 
 @receiver(post_save, sender=Product)
 def sync_producto(sender, instance, created, **kwargs):
-    try:
-        product_public = Product_public.objects.get(codigo=instance.codigo)
-        product_public.codigo = instance.codigo
-        product_public.name_extend = instance.name_extend
-        product_public.images = instance.images
-        product_public.image_alterna = instance.image_alterna
-        product_public.description = instance.description
-        product_public.price1 = instance.price1
-        product_public.price2 = instance.price2
-        product_public.price_old = instance.price_old
-        product_public.flag = instance.flag
-        product_public.ref = instance.ref 
-        product_public.slug = instance.slug 
-        product_public.active = instance.active       
-        product_public.soldout = instance.soldout 
-        product_public.offer = instance.offer 
-        product_public.home = instance.home
-        product_public.created_date = instance.created_date 
-        product_public.modified_date = instance.modified_date 
-        product_public.save()
-    except Product_public.DoesNotExist:
-        Product_public.objects.create(
-            codigo = instance.codigo,
-            name_extend = instance.name_extend,
-            images = instance.images,
-            image_alterna = instance.image_alterna,
-            description = instance.description,
-            price1 = instance.price1,
-            price2 = instance.price2,
-            price_old = instance.price_old,
-            flag = instance.flag,
-            ref = instance.ref ,
-            slug = instance.slug, 
-            active = instance.active,       
-            soldout = instance.soldout, 
-            offer = instance.offer, 
-            home = instance.home,
-            created_date = instance.created_date ,
-            modified_date = instance.modified_date,
-        )
-    except Exception as e:
-        print(f"Error en la señal post_save: {e}")
+    if created:
+        try:
+            product_public = Product_public.objects.get(codigo=instance.codigo)
+            product_public.codigo = instance.codigo
+            product_public.name_extend = instance.name_extend
+            product_public.images = instance.images
+            product_public.image_alterna = instance.image_alterna
+            product_public.description = instance.description
+            product_public.price1 = instance.price1
+            product_public.price2 = instance.price2
+            product_public.price_old = instance.price_old
+            product_public.flag = instance.flag
+            product_public.ref = instance.ref 
+            product_public.slug = instance.slug 
+            product_public.active = instance.active       
+            product_public.soldout = instance.soldout 
+            product_public.offer = instance.offer 
+            product_public.home = instance.home
+            product_public.created_date = instance.created_date 
+            product_public.modified_date = instance.modified_date 
+            product_public.save()
+        except:
+            product = Product_public.objects.create(
+                codigo = instance.codigo,
+                name_extend = instance.name_extend,
+                images = instance.images,
+                image_alterna = instance.image_alterna,
+                description = instance.description,
+                price1 = instance.price1,
+                price2 = instance.price2,
+                price_old = instance.price_old,
+                flag = instance.flag,
+                ref = instance.ref ,
+                slug = instance.slug, 
+                active = instance.active,       
+                soldout = instance.soldout, 
+                offer = instance.offer, 
+                home = instance.home,
+                created_date = instance.created_date ,
+                modified_date = instance.modified_date,
+            )
+            product.save()
+            return product       
+    else:
+        print("Se crearon cero registros")
 
 # # Señal para manejar la eliminación de Producto
 # @receiver(pre_delete, sender=Producto)
