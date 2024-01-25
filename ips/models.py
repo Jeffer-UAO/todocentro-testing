3dacc6d7094cb7450c4fe78904146a6f72c4ea86
JefferDevop <somadesarrollo@gmail.com>
2024-01-25 03:36:11 -0500

    Entradas de almacen 13

--- ips/models.py
+++ ips/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Ip(models.Model):
     TIPO = (
         ('SALDOS INICIALES', 'EA'),
-        ('ENTRADAS', 'E1')
+        ('ENTRADA', 'E1')
     )
 
     number = models.CharField(editable=False, max_length=20, verbose_name=(u'No. Documento'))
@@ -34,7 +34,7 @@ class Ip(models.Model):
     
     class Meta:
         verbose_name = "Entrada"
-        verbose_name_plural = "Entradas"
+        verbose_name_plural = "Entrada de mercancia"
 
     def __str__(self):
         return f"{self.number} - {self.tipo}"
