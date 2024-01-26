f6f6a51a5b291763c758c6185f78629e6852330d
JefferDevop <somadesarrollo@gmail.com>
2024-01-25 19:48:11 -0500

    Itemact 14

--- ips/admin.py
+++ ips/admin.py
@@ -7,6 +7,15 @@ class IpdetInline(admin.TabularInline):
     model = Ipdet
     extra = 1  # Puedes ajustar esto según tus necesidades
 
-@admin.register(Ip)
+
 class IpAdmin(admin.ModelAdmin):
-    inlines = [IpdetInline]
+    def get_readonly_fields(self, request, obj=None):
+        actions = super(Ip, self).get_actions(request)
+        return ['Tipo', 'Total']
+    actions= None
+    list_display = ('tipo', 'mumber', 'total', 'created_date')
+    search_fields = ('number','created_data')
+    list_display_links = None
+    list_per_page = 8
+    
+admin.site.register(Ip, IpAdmin)
