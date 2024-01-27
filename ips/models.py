4db2b7f51857b1bd439834d20af52a4f0bf92ec8
JefferDevop <somadesarrollo@gmail.com>
2024-01-27 07:42:15 -0500

    Restructuracuion de app inventario 32

--- ips/models.py
+++ ips/models.py
@@ -12,7 +12,7 @@ class Ip(models.Model):
     tipo = models.CharField(max_length=20, choices=TIPO)
     # cust = models.ForeignKey('custs.Tercero', on_delete=models.PROTECT, verbose_name=("Proveedor"))
     concept = models.CharField(max_length=80, verbose_name='Concepto', null=True, blank=True)
-    total = models.DecimalField(max_digits=9, decimal_places=2, default=0.00)
+    total = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
     created_date = models.DateTimeField(auto_now_add=True, null=True, blank=True, verbose_name=("Creado"))
 
 
@@ -49,9 +49,9 @@ class Ipdet(models.Model):
     number = models.PositiveIntegerField(editable=False, default=0)
     ip = models.ForeignKey(Ip, on_delete=models.CASCADE)     
     item = models.ForeignKey('products.Product', on_delete=models.CASCADE ,verbose_name="Item")
-    qty = models.SmallIntegerField(default=1, verbose_name=(u'Cantidad'))
-    cost = models.DecimalField(max_digits=9, decimal_places=2, blank=False, null=False, default= 0.0, verbose_name=(u'Costo'))
-    subtotal = models.DecimalField(max_digits=9, decimal_places=2, blank=False, null=False, default= 0.0, verbose_name=(u'SubTotal'))
+    qty = models.DecimalField(max_digits=9, decimal_places=2, blank=False, null=False, default= 0.0, verbose_name=(u'Cantidad'))
+    cost = models.DecimalField(max_digits=12, decimal_places=2, blank=False, null=False, default= 0.0, verbose_name=(u'Costo'))
+    subtotal = models.DecimalField(max_digits=12, decimal_places=2, blank=False, null=False, default= 0.0, verbose_name=(u'SubTotal'))
     comments = models.CharField(max_length=100, blank=True, verbose_name=(u'Comentario'))
     
     
@@ -81,7 +81,7 @@ class Ipdet(models.Model):
 
 class Itemact(models.Model):   
     ipdet = models.ForeignKey(Ipdet, on_delete=models.CASCADE)        
-    qty = models.SmallIntegerField(default=0)
+    qty = models.DecimalField(max_digits=9, decimal_places=2, default= 0)
     tipo = models.CharField(editable=False, max_length=20, null=True, blank=True)
     number = models.PositiveIntegerField(editable=False, default=0)
     item = models.ForeignKey('products.Product', on_delete=models.CASCADE)
@@ -96,18 +96,18 @@ class Itemact(models.Model):
 
 class ItemactItem(models.Model):   
     item = models.ForeignKey('products.Product', on_delete=models.CASCADE, null=True, blank=True, default="")
-    cantidad_actual = models.SmallIntegerField(default=0)
+    cantidad_actual = models.DecimalField(max_digits=9, decimal_places=2, default= 0)
     nombre = models.CharField(max_length=200, blank=True, null=True)
     uuid = models.UUIDField(editable=False, blank=True, null=True)
     images = models.CharField(max_length=600, default="", blank=True)
     image_alterna = models.CharField(max_length=600, default="", blank=True)
     description = models.TextField(max_length=2000, blank=True, default="")
-    price1 = models.DecimalField(max_digits=9, decimal_places=2, default= 0.0)
-    price2 = models.DecimalField(max_digits=9, decimal_places=2, default= 0.0)
-    price_old = models.DecimalField(max_digits=9, decimal_places=2, default= 0.0)
+    price1 = models.DecimalField(max_digits=12, decimal_places=2, default= 0.0)
+    price2 = models.DecimalField(max_digits=12, decimal_places=2, default= 0.0)
+    price_old = models.DecimalField(max_digits=12, decimal_places=2, default= 0.0)
     flag = models.CharField(max_length=200, blank=True, default="")
     ref = models.CharField(max_length=200, blank=True, default="")
-    qty = models.DecimalField(max_digits=5, decimal_places=2, default= 0.0)
+    qty = models.DecimalField(max_digits=9, decimal_places=2, default= 0)
     slug = models.CharField(max_length=200, blank=True, default="")
     active = models.CharField(max_length=5, blank=True, default="")
     soldout = models.CharField(max_length=5, blank=True, default="")
