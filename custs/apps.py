4b59d2fd1700f0d1a9bcaeb93cc4372843c938eb
JefferDevop <somadesarrollo@gmail.com>
2024-01-28 06:52:00 -0500

    Administracion de terceros 1

--- custs/apps.py
+++ custs/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class CustsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'custs'
+    verbose_name = 'Administracion de Terceros'
