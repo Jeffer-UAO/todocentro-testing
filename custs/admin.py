dbcd7d38e6b266f086c7ee78ccbd4e85c8829586
JefferDevop <somadesarrollo@gmail.com>
2024-01-28 06:43:41 -0500

    Administracion de terceros 1

--- custs/admin.py
+++ custs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Tercero
 
-# Register your models here.
+class TerceroAdmin(admin.ModelAdmin):    
+    list_display = ('document', 'name', 'addres', 'phone', 'email', 'cust')
+    list_display_links = ('document', 'name', 'addres', 'phone', 'email')
+    search_fields = ('document', 'name', 'addres', 'phone', 'email',)   
+    list_per_page = 10
+    
+admin.site.register(Tercero, TerceroAdmin)
