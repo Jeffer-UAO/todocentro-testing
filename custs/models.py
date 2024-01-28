0352352ebbf6fb5a277cea9ac49520b8126f1d43
JefferDevop <somadesarrollo@gmail.com>
2024-01-28 07:05:59 -0500

    Administracion de terceros 2

--- custs/models.py
+++ custs/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 class Tercero(models.Model):    
-    name = models.CharField(max_length=20, verbose_name=(u'Nombre'))
+    name = models.CharField(max_length=120, verbose_name=(u'Nombre'))
     document = models.IntegerField(verbose_name='Nit')  # TODO: validar que sea un DNI o CUIT válido
-    addres = models.TextField(blank=True, verbose_name='Dirección')
+    addres = models.CharField(max_length=120, blank=True, verbose_name='Dirección')
     phone = models.CharField(max_length=15, blank=True, verbose_name='Teléfono')
     email = models.EmailField(blank=True, verbose_name='Correo')
-    cust = models.BooleanField(default=True, verbose_name='Proveedor')
+    supplier = models.BooleanField(default=True, verbose_name='Proveedor')
+    cust = models.BooleanField(default=False, verbose_name='Cliente')
     
 
     class Meta:
