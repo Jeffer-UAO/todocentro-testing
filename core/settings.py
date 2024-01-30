7fd972fd064ca3e3d618556d81ca16279a429857
JefferDevop <somadesarrollo@gmail.com>
2024-01-30 18:32:46 -0500

    Get up tenant the instance and set in campo dominio in model 5

--- core/settings.py
+++ core/settings.py
@@ -53,7 +53,7 @@ TENANT_APPS = (
 )
 
 INSTALLED_APPS = list(SHARED_APPS) + \
-    [app for app in TENANT_APPS if app not in SHARED_APPS]
+    [app for app in TENANT_APPS if app  in SHARED_APPS]
 
 
 MIDDLEWARE = [
