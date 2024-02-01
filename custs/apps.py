1c4cb8209949e1fd37fb1d5e4c092a09161388cf
JefferDevop <somadesarrollo@gmail.com>
2024-02-01 07:40:04 -0500

    Error en las entradas de almacen y order

--- custs/apps.py
+++ custs/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class CustsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'custs'
-    verbose_name = 'Administración de Terceros'
+    verbose_name = '3. Administración de Terceros'
