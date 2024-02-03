8f209a20e149076eb7274207a7d1b24b75f7c0f2
JefferDevop <somadesarrollo@gmail.com>
2024-02-03 16:26:15 -0500

    Control de reservas y stock

--- orders/api/serializers.py
+++ orders/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 from ..models import Order, Orderdet
+from rest_framework import serializers
 
 
 
@@ -9,12 +10,16 @@ class OrderdetSerializer(ModelSerializer):
         fields = ["comments", "price", "qty"]
 
 
-class OrderSerializer(ModelSerializer):
-    orderdetData = OrderdetSerializer(source='order_id', read_only=True, many=True)
+class OrderSerializer(serializers.ModelSerializer):
+    orderdetData = serializers.SerializerMethodField()
+
     class Meta:
         model = Order
         fields = ["number", "cust", "tipo", "total", "created_date", "concept", "orderdetData"]
 
-
+    def get_orderdetData(self, obj):
+        orderdet_instances = obj.orderdet_set.all()  # Asegúrate de que el nombre sea correcto según tu modelo de datos
+        orderdet_serializer = OrderdetSerializer(orderdet_instances, many=True)
+        return orderdet_serializer.data
 
 
